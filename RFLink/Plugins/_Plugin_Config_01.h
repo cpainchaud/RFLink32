7be1165f599be62650ce04d6abc26cfdc51b5df3
couin3 <35873376+couin3@users.noreply.github.com>
2020-04-02 11:34:44 +0200

    Adding 061 & 062

--- RFLink/Plugins/_Plugin_Config_01.h
+++ RFLink/Plugins/_Plugin_Config_01.h
@@ -54,8 +54,8 @@
 // Motion Sensors, include when needed
 // -------------------
 // #define PLUGIN_060              // Ajax Chubb Varel 433 MHz. motion sensors
-// #define PLUGIN_061              // Chinese PIR motion door and window sensors
-// #define PLUGIN_062              // Chuango Alarm Devices
+#define PLUGIN_061 // Chinese PIR motion door and window sensors
+#define PLUGIN_062 // Chuango Alarm Devices
 // #define PLUGIN_063              // Oregon PIR/ALARM/LIGHT
 // -------------------
 // Doorbells
