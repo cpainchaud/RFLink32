2d03d971df89daedb14c84bc2e48c78be84a84f6
couin3 <35873376+couin3@users.noreply.github.com>
2020-04-10 22:19:42 +0200

    Added display_XXX func

--- RFLink/Plugins/Plugin_033.c
+++ RFLink/Plugins/Plugin_033.c
@@ -31,22 +31,28 @@
  * Sample:
  * 20;8D;DEBUG;Pulses=80;Pulses(uSec)=1890,5760,1890,5730,1890,5760,1890,5730,1890,5760,1890,5760,1890,5760,1890,5760,1890,5760,1890,5760,1890,5760,5910,1830,1890,5640,1890,5760,5910,1830,1890,5640,5910,1830,1860,5640,5910,1830,1890,5610,5910,1830,5910,1830,5910,1830,1890,5400,5910,1830,1890,5610,1890,5760,5910,1830,5910,1830,1890,5520,1890,5760,5910,1860,1890,5610,1890,5760,1890,5760,5910,1830,1890,5610,5910,1830,5910,1830,1860,6990;
  \*********************************************************************************************/
+#define CRESTA_PLUGIN_ID 34
 #define CONRAD_PULSECOUNT 80
+
 #define CONRAD_PULSEMAX 5000 / RAWSIGNAL_SAMPLE_RATE
 #define CONRAD_PULSEMIN 2300 / RAWSIGNAL_SAMPLE_RATE
 
 #ifdef PLUGIN_033
+#include "../4_Misc.h"
+
 boolean Plugin_033(byte function, char *string)
 {
    if (RawSignal.Number != CONRAD_PULSECOUNT)
       return false;
+
    unsigned long bitstream = 0L;
-   unsigned int temperature = 0;
-   unsigned int rc = 0;
    byte checksum = 0;
    byte bitcount = 0; // bit counter (counting first 8 bits that need
+   unsigned int rc = 0;
+   unsigned int temperature = 0;
    //==================================================================================
-   // get all 28 bits
+   // Get all 28 bits
+   //==================================================================================
    for (byte x = 1; x <= CONRAD_PULSECOUNT - 1; x += 2)
    {
       if (RawSignal.Pulses[x] > CONRAD_PULSEMAX)
@@ -54,60 +60,63 @@ boolean Plugin_033(byte function, char *string)
          if (RawSignal.Pulses[x + 1] > CONRAD_PULSEMAX)
             if ((x + 1) < CONRAD_PULSECOUNT)
                return false; // invalid pulse length
+
          if (bitcount > 7)
          {
-            bitstream = (bitstream << 1) | 0x1;
+            bitstream <<= 1;
+            bitstream |= 0x1;
          }
          else
-         {
             return false; // first 8 bits should all be zeros
-         }
          bitcount++;
       }
       else
       {
          if (RawSignal.Pulses[x] > CONRAD_PULSEMIN)
             return false; // invalid pulse length
+
          if (RawSignal.Pulses[x + 1] < CONRAD_PULSEMIN)
             return false; // invalid pulse length
+
          if (bitcount > 7)
-            bitstream = (bitstream << 1);
+            bitstream <<= 1;
+
          bitcount++;
       }
    }
    //==================================================================================
+   // Perform a quick sanity check
+   //==================================================================================
+   if (bitstream == 0)
+      return false;
+   //==================================================================================
    // Prevent repeating signals from showing up
    //==================================================================================
-   Serial.println(bitstream, HEX);
-   if ((SignalHash != SignalHashPrevious) || (RepeatingTimer + 1000 < millis()) || (SignalCRC != bitstream))
-   {
+   if ((SignalHash != SignalHashPrevious) || (RepeatingTimer + 500 < millis()) || (SignalCRC != bitstream))
       SignalCRC = bitstream; // not seen the RF packet recently
-      if (bitstream == 0)
-         return false; // Perform a sanity check
-   }
    else
-   {
       return true; // already seen the RF packet recently
-   }
    //==================================================================================
-   checksum = (bitstream >> 4) & 0x0f;
-   if (checksum != 0x01)
+   // Perform checksum calculations
+   //==================================================================================
+   checksum = (bitstream >> 4) & 0xF;
+   if (checksum != 0x1)
       return false;
 
-   rc = (bitstream >> 24);
-   temperature = (bitstream >> 14) & 0x3ff;
+   //==================================================================================
+   // Now process the various sensor types
+   //==================================================================================
+   rc = ((bitstream >> 24) & 0xFF);
+   temperature = ((bitstream >> 14) & 0x3FF);
    temperature = temperature - 500;
    //==================================================================================
    // Output
-   // ----------------------------------
-   sprintf(pbuffer, "20;%02X;", PKSequenceNumber++); // Node and packet number
-   Serial.print(pbuffer);
-   Serial.print(F("Conrad;"));       // Label
-   sprintf(pbuffer, "ID=%04x;", rc); // ID
-   Serial.print(pbuffer);
-   sprintf(pbuffer, "TEMP=%04x;", temperature);
-   Serial.print(pbuffer);
-   Serial.println();
+   //==================================================================================
+   display_Header();
+   display_Name(PSTR("Conrad"));
+   display_IDn(rc, 4);
+   display_TEMP(temperature);
+   display_Footer();
    //==================================================================================
    RawSignal.Repeats = true; // suppress repeats of the same RF packet
    RawSignal.Number = 0;
