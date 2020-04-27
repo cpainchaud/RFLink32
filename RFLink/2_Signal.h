481c778773f4e0ab6738d56ff0c9a0564cddce8b
couin3 <35873376+couin3@users.noreply.github.com>
2020-04-28 00:54:44 +0200

    Moved signal specific define to 2_Signal.h

--- RFLink/2_Signal.h
+++ RFLink/2_Signal.h
@@ -9,7 +9,16 @@
 #define Signal_h
 
 #include <Arduino.h>
-#include "RFLink.h"
+
+#define RAW_BUFFER_SIZE 292         // 292        // Maximum number of pulses that is received in one go.
+#define MIN_RAW_PULSES 50           // 50         // Minimal number of bits that need to have been received before we spend CPU time on decoding the signal.
+#define RAWSIGNAL_SAMPLE_RATE 32    // 32         // =8 bits. Sample width / resolution in uSec for raw RF pulses.
+#define SIGNAL_SEEK_TIMEOUT_MS 25   // 25         // After this time in mSec, RF signal will be considered absent.
+#define SIGNAL_MIN_PREAMBLE_US 3000 // 3000       // After this time in mSec, a RF signal will be considered to have started.
+#define MIN_PULSE_LENGTH_US 100     // 25!        // Pulses shorter than this value in uSec. will be seen as garbage and not taken as actual pulses.
+#define SIGNAL_END_TIMEOUT_US 5000  // 4500       // After this time in uSec, the RF signal will be considered to have stopped.
+#define SIGNAL_REPEAT_TIME_MS 250   // 500        // Time in mSec. in which the same RF signal should not be accepted again. Filters out retransmits.
+#define SCAN_HIGH_TIME_MS 50        // 50         // time interval in ms. fast processing for background tasks
 
 struct RawSignalStruct // Raw signal variabelen places in a struct
 {
--- RFLink/RFLink.h
+++ RFLink/RFLink.h
@@ -12,15 +12,6 @@
 #define BUILDNR 0x04                    // 0x07       // shown in version
 #define REVNR 0x00                      // 0X42       // shown in version and startup string
 #define BAUD 57600                      // 57600      // Baudrate for serial communication.
-#define MIN_RAW_PULSES 50               // 50         // Minimal number of bits that need to have been received before we spend CPU time on decoding the signal.
-#define RAW_BUFFER_SIZE 292             // 292        // Maximum number of pulses that is received in one go.
-#define RAWSIGNAL_SAMPLE_RATE 32        // 32         // =8 bits. Sample width / resolution in uSec for raw RF pulses.
-#define SIGNAL_SEEK_TIMEOUT_MS 25       // 25         // After this time in mSec, RF signal will be considered absent.
-#define SIGNAL_MIN_PREAMBLE_US 3000     // 3000       // After this time in mSec, a RF signal will be considered to have started.
-#define MIN_PULSE_LENGTH_US 100         // 25!        // Pulses shorter than this value in uSec. will be seen as garbage and not taken as actual pulses.
-#define SIGNAL_END_TIMEOUT_US 5000      // 4500       // After this time in uSec, the RF signal will be considered to have stopped.
-#define SIGNAL_REPEAT_TIME_MS 250       // 500        // Time in mSec. in which the same RF signal should not be accepted again. Filters out retransmits.
-#define SCAN_HIGH_TIME_MS 50            // 50         // time interval in ms. fast processing for background tasks
 #define FOCUS_TIME_MS 50                // 50         // Duration in mSec. that, after receiving serial data from USB only the serial port is checked.
 #define PLUGIN_MAX 55                   // 55         // Maximum number of Receive plugins
 #define PLUGIN_TX_MAX 5                 // 26         // Maximum number of Transmit plugins
