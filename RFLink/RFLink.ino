1f2cdc07169ab970518a539673df8cb51b5bee2b
Christophe Painchaud <Christophe Painchaud>
2021-02-01 11:49:01 +0100

    Async Receiver copyright update

--- RFLink/RFLink.ino
+++ RFLink/RFLink.ino
@@ -3,6 +3,7 @@
 // * Portions Free Software 2018..2020 StormTeam - Marc RIVES
 // * Portions Free Software 2015..2016 StuntTeam - (RFLink R29~R33)
 // * Portions © Copyright 2010..2015 Paul Tonkes (original Nodo 3.7 code)
+// * Portions GPLv3 2021 Christophe Painchaud Async Receiver (Async Receiver)
 // *
 // *                                       RFLink-esp
 // *
