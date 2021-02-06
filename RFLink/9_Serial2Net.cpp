19c791162a53079bab5a94e073eceb5afc134a29
Christophe Painchaud <Christophe Painchaud>
2021-02-06 20:18:28 +0100

    Serial2Net : support read data sent by remote client

--- RFLink/9_Serial2Net.cpp
+++ RFLink/9_Serial2Net.cpp
@@ -3,12 +3,105 @@
 
 
 namespace RFLink { namespace Serial2Net {
+
+    class Serial2NetClient : public WiFiClient {
+
+        private:
+            static const uint16_t __buffer_size = 64;
+            char buffer[__buffer_size+1];
+            uint16_t end = 0;
+
+        public:
+            bool ignore = true;
+
+            Serial2NetClient(): WiFiClient::WiFiClient(){
+                buffer[__buffer_size] = 0;
+            }
+
+            Serial2NetClient & operator=(const WiFiClient &other)  {
+                WiFiClient::operator=(other);
+                ignore = false;
+                end = 0;
+                return *this;
+            }
+
+            /**
+             * @return -1 if not found or error, index of last character of the commeand if found
+             * */
+            int hasCommandAvailable() {
+                #if defined(SERIAL2NET_DEBUG) || defined(DEBUG)
+                String debugmsg;
+                #endif
+
+                int newBytesCount = available();
+                
+                if( !connected() ) { // some errors happened during read operations
+                    ignore = true;
+                    return -1;
+                }
+
+                if(newBytesCount < 1)
+                    return -1;
+
+                #if defined(SERIAL2NET_DEBUG) || defined(DEBUG)
+                    debugmsg += "Serial2Net: client has " + String(newBytesCount) + " more bytes to available to read";
+                    Serial.println(debugmsg);
+                #endif
+
+                uint16_t spaceLeft = __buffer_size - end;
+
+                if(newBytesCount > spaceLeft)
+                    newBytesCount = spaceLeft;
+
+                readBytes(buffer + end, newBytesCount);
+
+                int result = -1;
+
+                for(int i=end; i< end+newBytesCount; i++) {
+                    if(buffer[i] == 0x0d) {
+                        result = i-1;
+                        #if defined(SERIAL2NET_DEBUG) || defined(DEBUG)
+                            debugmsg += "Serial2Net: end of command found at position " + String(result) ;
+                            Serial.println(debugmsg);
+                        #endif
+                    }
+                }
+                end += newBytesCount;
+
+                if( result < 0 && end >= __buffer_size-1 ){
+                    println("Command is too long, we're closing this connection!");
+                    ignore = true;
+                    stop();
+                    return -1;
+                }
+
+                return result;
+            }
+
+            /**
+             * Removes the command of which end at given position from the buffer (does not include new line character)
+             * */
+            String consumeCommand(uint16_t position){
+                buffer[position+1] = 0;
+                String result(buffer);
+
+                if (buffer[position+2] == 0x0A) {
+                    memcpy(buffer, buffer+position+3, end-position-2);
+                    end = end - position -3;
+                }
+                else{
+                    memcpy(buffer, buffer+position+2, end-position-1);
+                    end = end - position -2;
+                }
+                return result;
+            }
+    };
     
     WiFiServer server;
 
     boolean alreadyConnected = false;
     const unsigned short clientsMax=10;
-    WiFiClient clients[clientsMax];
+    Serial2NetClient clients[clientsMax];
 
     inline bool isNewClient(WiFiClient &client) {
         for(int i=0; i<clientsMax; i++) {
@@ -40,34 +133,33 @@ namespace RFLink { namespace Serial2Net {
 
     void serverLoop(){
         #if defined(SERIAL2NET_DEBUG) || defined(DEBUG)
-        String debugmsg("Serial2Net: ");
+        String debugmsg;
         #endif
 
         WiFiClient client = server.available();
 
         if( isNewClient(client) ) {
             #if defined(SERIAL2NET_DEBUG) || defined(DEBUG)
-            debugmsg += "Serial2Net: new client detected: " + client.remoteIP().toString() + " port:" + client.remotePort();
+            debugmsg += "Serial2Net: new client detected IP=" + client.remoteIP().toString() + " port=" + client.remotePort();
             Serial.println(debugmsg);
             #endif
             registerClient(client);
             return;
         }
 
-        //from here we have a client sending data
-        char buffer[256];
-        int readMax = client.available();
-        if (readMax > 0) {
-            if (readMax > 256)
-                readMax = 256; 
-            client.readBytesUntil(0x0d, buffer, readMax);
-
-            #if defined(SERIAL2NET_DEBUG) || defined(DEBUG)
-                debugmsg += "Serial2Net: client sent data: ";
-                debugmsg += buffer;
-                Serial.println(debugmsg);
-            #endif
-        } 
+        // Let's see if any client has sent some data
+        for(int i=0; i<clientsMax; i++) {
+            if(!clients[i].ignore) {
+                int commandLastCharPos = clients[i].hasCommandAvailable();
+                if (commandLastCharPos >=0) {
+                    String command = clients[i].consumeCommand(commandLastCharPos);
+                    #if defined(SERIAL2NET_DEBUG) || defined(DEBUG)
+                    debugmsg += "Serial2Net: client has sent a command >>>> " + command + " <<<<<<";
+                    Serial.println(debugmsg);
+                    #endif
+                }
+            }
+        }
     }
 
     void broadcastMessage(const char *msg) {
