ff5abff6f5b40401dc3e5ba584ec2c2f18fbc3c4
OBones <obones@free.fr>
2021-04-06 10:50:41 +0200

    str2cmd should take a const argument as it does not touch it, and it allows using const values when calling it

--- RFLink/4_Display.h
+++ RFLink/4_Display.h
@@ -103,7 +103,7 @@ boolean retrieve_End();
 #define VALUE_LIMIT 82
 #define VALUE_ALLON 141
 
-int str2cmd(char *);
+int str2cmd(const char *);
 void replacechar(char *, char, char);
 
 #if (defined(ESP8266) || defined(ESP32))
